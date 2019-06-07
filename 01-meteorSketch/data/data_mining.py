a76f37138613e16574439d3cd0a07dc4196ca6cc
Bruno Castro <contato@brunocastro.me>
2019-06-07 15:52:24 -0300

    data-mining update.

--- 01-meteorSketch/data/data_mining.py
+++ 01-meteorSketch/data/data_mining.py
@@ -1,6 +1,5 @@
 # importing pandas dataframing 
 import pandas as pd 
-import datetime
 
 # parse gross data of csv on a specific columns
 meteorList = pd.read_csv("gross_data.csv", usecols = ['name','recclass','mass','fall','year','reclat','reclong'])
@@ -8,9 +7,5 @@ meteorList = pd.read_csv("gross_data.csv", usecols = ['name','recclass','mass','
 # Sort Meteor List with Mass
 meteorList.sort_values(by='mass', ascending=False, inplace=True)
 
-# for row in meteorList:
-#     memory = row['year'].strftime("%m/%y")
-#     print(memory)
-
 # printing first 10 rows 
-print(meteorList[:10]['mass'])
+print(meteorList[:20])
