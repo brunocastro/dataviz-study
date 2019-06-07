b1e0fea33a2eb44e91927211c86ba580cc57353a
Bruno Castro <contato@brunocastro.me>
2019-06-07 20:57:06 -0300

    on datamining: Clear trash data and selecting only meteors with valid weighing and location.

--- 01-meteorSketch/data/data_mining.py
+++ 01-meteorSketch/data/data_mining.py
@@ -4,8 +4,23 @@ import pandas as pd
 # parse gross data of csv on a specific columns
 meteorList = pd.read_csv("gross_data.csv", usecols = ['name','recclass','mass','fall','year','reclat','reclong'])
 
+# remove mass NaN
+meteorList.dropna(subset=['mass','reclat','reclong'], inplace=True)
+
+# Setup float format and revert Nunbers notation on mass
+pd.options.display.float_format = '{:,.4f}'.format
+meteorList["mass"] = meteorList["mass"].astype(int)
+
 # Sort Meteor List with Mass
 meteorList.sort_values(by='mass', ascending=False, inplace=True)
 
-# printing first 10 rows 
-print(meteorList[:20])
+clearData = meteorList.loc[(meteorList["fall"]=="Found")]
+
+# #Savind the mining results
+clearData.to_csv('MeteorStrikes.csv')
+
+# #plot comand-line feed back
+print ('All done! See the sample:')
+
+# printing first 20 rows 
+print (clearData[:20])
